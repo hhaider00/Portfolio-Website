628648ab5f467ccace9821e388cf0eea74eb30dc
Hamza <78364172+pfn19@users.noreply.github.com>
2022-09-21 17:55:44 +0100

    fixing portfolio page

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,31 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import Home from "./home";
+import Login from "../Login";
 
 
 const Dashboard = () =>
 {
+
+    const [user,setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(()=>
+    {
+        onAuthStateChanged(auth,(user) => {
+            if(user)
+            {
+                setUser(user);
+            }
+            else
+            {
+                setUser(null);
+            }
+        })
+    },[]);
     return(
         <div >
-            hi
+            {user? <Home/> : <Login/>}
         </div>
     )
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,13 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
-import portfolioData from '../../data/portfolio.json';
-
+import { getDocs, collection } from 'firebase/firestore/lite';
+import { db } from '../../firebase';
 const Portfolio = () =>
 {
     var title = "Portfolio"
     const [letterClass, setLetterClass] = useState('text-animate')
+    const [portfolio, setPortfolio] = useState([]);
     useEffect(() => { setTimeout(() => { setLetterClass('text-animate-hover')}, 6000)},[])
+
+    useEffect(() => {
+        getPorfolio();
+    },[]);
+    const getPorfolio = async () =>
+    {
+        const querySnapShot = await getDocs(collection(db, 'portfolio'));
+        setPortfolio(querySnapShot.docs.map((doc) => doc.data()));
+        
+    }
 
     const renderPortfolio = (portfolio) =>
     {
@@ -18,9 +29,9 @@ const Portfolio = () =>
                     portfolio.map((port,index) => {
                         return(
                             <div className="image-box" key={index}>
-                                <img className="portfolio-image" src={port.cover} alt="portfolio"/>
+                                <img className="portfolio-image" src={port.image} alt="portfolio"/>
                                 <div className="content">
-                                    <p className="title">{port.title}</p>
+                                    <p className="title">{port.name}</p>
                                     <h4 className="description">{port.description}</h4>
                                     <button className="button" onClick={() => window.open(port.url)}>Veiw</button>
                                 </div>
@@ -40,7 +51,7 @@ const Portfolio = () =>
             <h1 className="page-title">
                 <AnimatedLetters  index={15} strArray={title.split("")} letterClass={letterClass}/>
             </h1>
-            <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            <div>{renderPortfolio(portfolio)}</div>
         </div>
        
         <Loader type="ball-scale-ripple-multiple"/>
